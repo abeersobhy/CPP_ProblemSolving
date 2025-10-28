47b0c3dd398d8782586e936d380ffc1cc28524fc
Abeer <abeer.sobhy.eg@gmail.com>
2025-10-28 21:06:27 +0300

    Description comment added to problem3

--- problem3/Is_N_Divisible_By_xAndy.cpp
+++ problem3/Is_N_Divisible_By_xAndy.cpp
@@ -1,3 +1,8 @@
+/*
+ Create a function that:
+ Checks if a number n is divisible by two numbers x AND y.
+ All inputs are positive, non-zero numbers.
+*/
 #include <iostream>
 bool isDivisible(int n, int x, int y);
 
